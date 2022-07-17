9d5584269bc29dff20d698b322c0ee19f6d22587
Matheus Lima <matheuscaldas95@gmail.com>
2022-07-17 11:18:02 -0300

    feature(header): New header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,100 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
 import { MainDiv, ListaLinks } from './styles';
 
 const Header: React.FC = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
-        <>
-            <MainDiv>
-                <h3>Fake Detector</h3>
-                <ListaLinks>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/sobre">Sobre</Link>
-                    </li>
-                    <li>
-                        <Link to="/servicos">Serviços</Link>
-                    </li>
-                </ListaLinks>
-            </MainDiv>
-        </>
+        <AppBar position='static'>
+            <Container>
+                <Toolbar disableGutters>
+                    <Typography
+                        variant='h6'
+                        noWrap
+                        component="a"
+                        href='/'
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Fake Detector
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            <MenuItem onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">Sobre</Typography>
+                            </MenuItem>
+                            <MenuItem onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">Serviços</Typography>
+                            </MenuItem>
+
+                        </Menu>
+
+                    </Box>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        <Button
+                            onClick={handleCloseNavMenu}
+                            href="/sobre"
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            Sobre
+                        </Button>
+                        <Button
+                            onClick={handleCloseNavMenu}
+                            href="/servicos"
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            Serviços
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
