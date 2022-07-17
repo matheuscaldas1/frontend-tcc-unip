1101d2a21ee2551514cecf55e85675fe602dece3
Matheus Lima <matheuscaldas95@gmail.com>
2022-07-17 13:17:47 -0300

    feature(servicos): Config do form de analise

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,16 +1,14 @@
-import styled from "styled-components";
+import { styled } from "@material-ui/styles";
+import {
+    Typography
+} from "@mui/material";
 
-export const MainDiv = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #fdfdfd;
-`;
-
-export const ListaLinks = styled.ul`
-    display: flex;
-    justify-content: space-around;
-    width: 20%;
-    list-style-type: none;
-`
+export const CustomizedTypography = styled(Typography)({
+    textDecoration: 'none',
+    mr: 2,
+    fontFamily: 'monospace',
+    fontWeight: 700,
+    letterSpacing: '.3rem',
+    color: 'inherit',
+    display: 'flex',
+});
